2d8f8ccafb06782d6ea1dc53d488bac9c94050f4
Nadeera Hashan Kuruppu <37404014+lucifer955@users.noreply.github.com>
2019-09-20 18:44:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// add new todo
+
+const addNew = document.querySelector('.addTodo');
+const addToList = document.querySelector('.todos'); 
+
+// template off the new todo
+const generateTemplate = newTodo => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${newTodo}</span>
+                <i class="fa fa-trash delete"></i>
+    </li>
+    `;
+    addToList.innerHTML+=html;
+
+}
+
+addNew.addEventListener('submit', e =>{
+
+    // stops the default action of an element from happening
+    e.preventDefault(); 
+
+    // get the input value
+    const newTodo = addNew.addNewTodo.value.trim();
+    
+    // create a template to add new todo to the list
+    generateTemplate(newTodo);
+    // reset the input form
+    addNew.reset();
+});
+
+
+// delete the todo
+
+addToList.addEventListener('click', e=>{
+    // check whether it is click only on the trash icon
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
